ea77950bca51537ee69049e3cd22b0f094e28184
astropaula <wojciechowska.fran@gmail.com>
2020-08-27 13:04:06 +0200

    Updates

---
+++ precache-manifest.83c858107a3a7d075cc55c0dab9c8366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5686f34553018bddbdac0bfedabc0ae5",
+    "url": "/picgram/index.html"
+  },
+  {
+    "revision": "5a19fe67c0dcafaedd5c",
+    "url": "/picgram/static/css/main.5c37b369.chunk.css"
+  },
+  {
+    "revision": "ae4a8cd7aad6c112f760",
+    "url": "/picgram/static/js/2.71cfd4dd.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/picgram/static/js/2.71cfd4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a19fe67c0dcafaedd5c",
+    "url": "/picgram/static/js/main.de3a38e2.chunk.js"
+  },
+  {
+    "revision": "1e072a6717e9c06b35f7",
+    "url": "/picgram/static/js/runtime-main.a7830163.js"
+  }
+]);
